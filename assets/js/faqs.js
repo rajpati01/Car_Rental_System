6cbd40b30cd32d6de9089c5176862d39b176026e
rajpati <rajpatibhandari01@gmail.com>
2024-07-28 13:11:08 +0545

    added FAQs page with with responsiveness and JS

---
+++ assets/js/faqs.js
@@ -0,0 +1,16 @@
+// function to toggle FAQs answer
+
+document.addEventListener("DOMContentLoaded", function () {
+  var faqQuestions = document.querySelectorAll(".faq-question");
+
+  faqQuestions.forEach(function (question) {
+    question.addEventListener("click", function () {
+      var answer = this.nextElementSibling;
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+      } else {
+        answer.style.display = "block";
+      }
+    });
+  });
+});
